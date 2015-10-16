bccbf5325bf818002e510ea84d416f8b06001e67
Alvin <alvin.zablan62@myhunter.cuny.edu>
2015-10-16 19:58:01 -0400

    react up and running, tldr: fuck react

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./facebookLogin.js');
+require('./shleemStream.js');
 
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('example')
-// );
+
+
 console.log('hello');
