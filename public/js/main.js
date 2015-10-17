f3daa2747e17a85a35f6eb0df2b85d5cf6f289f2
amaldare93 <amaldare93@gmail.com>
2015-10-16 20:38:34 -0400

    add material-design-lite

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,8 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
-require('./facebookLogin.js');
+// var MDL = require('material-design-lite');
+// require('./facebookLogin.js');
 require('./shleemStream.js');
 
-
-
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
 console.log('hello');
