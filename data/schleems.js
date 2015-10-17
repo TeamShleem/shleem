946055100fcc577e1fa79b5fc2d71c11e763459f
andrewmilo <amiloslavsky@gmail.com>
2015-10-17 00:26:42 -0400

    add cheese

---
+++ data/schleems.js
@@ -0,0 +1,17 @@
+var schleems = [
+	
+{'name': 'Alvin Z', 'text': 'Check out this sweet song.', 'media': 'https://soundcloud.com/rafriley/50-cent-raf-riley-preview', '': 's'},
+{'name': 'Andrew M', 'text': 'Check out this drop.', 'media': 'https://soundcloud.com/happy_trails/sets/shleems', 'type': 's'},
+{'name': 'Himanshu T', 'text': 'Holy Sh**.', 'media': 'https://soundcloud.com/bukezfinezt/bukez-finezt-luigis-lowrider-free-download', 'type': 's'},
+{'name': 'Anthony M', 'text': 'Woah.', 'media': 'https://soundcloud.com/outtallectuals/deep-sea-creatures-01-refraq-ascend-descend', 'type': 's'},
+{'name': 'Adriana W', 'text': 'Just wait for it...', 'media': 'https://soundcloud.com/chrome-sparks/moonraker-1', 'type': 's'},
+{'name': 'Eric S', 'text': 'Check out my new single.', 'media': 'https://soundcloud.com/absrdst/marble-aesthetics-marble-zone-remix', 'type': 's'},
+{'name': 'Justin T', 'text': 'Check out this drop.', 'media': 'https://soundcloud.com/flofills/allupinit', 'type': 's'},
+{'name': 'Konstantinos P', 'text': 'Check out this drop.', 'media': 'https://soundcloud.com/gallant/open-up', 'type': 's'},
+{'name': 'Anna W', 'text': 'Check out this drop.', 'media': 'https://soundcloud.com/mr_carmack/unfocusedfocused', 'type': 's'},
+{'name': 'Pavel S', 'text': 'Check out this drop.', 'media': 'https://soundcloud.com/tommisch/lianne-la-havas-what-you-dont-do-tom-misch-remix', 'type': 's'},
+{'name': 'Ilya K', 'text': 'Check out this drop.', 'media': 'https://soundcloud.com/flofills/allupinit', 'type': 's'},
+	
+];
+
+module.exports = schleems;
