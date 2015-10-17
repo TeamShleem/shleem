c229af7d75a18f984b5cf633d922442b7117c736
Alvin <alvin.zablan62@myhunter.cuny.edu>
2015-10-16 22:35:53 -0400

    add shleem react classes

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./facebookLogin.js');
-require('./shleemStream.js');
+require('./react_classes/shleemStream.js');
 
 
 
---
+++ public/js/react_classes/shleemStream.js
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var data = [
+  {name: "Rick", text: "You are suppose to turn the boots on."},
+  {name: "Morty", text: ":("}
+];
+
+var Shleem = React.createClass({
+  	render: function() {
+	    return (
+	    	<div className="shleem">
+	        	<h2>I am a shleem within a shleemStream...</h2>
+	        	Name: {this.props.author}
+	      	</div>
+	    );
+ 	}
+});
+
+var ShleemStream = React.createClass({
+    render: function() {
+  //   	var data = [
+		//   {name: "Rick", text: "You are suppose to turn the boots on."},
+		//   {name: "Morty", text: ":("}
+		// ];
+        return (
+      		<div className="shleemStream">
+      			<h1>I am the shleemStream!</h1>
+      			<Shleem author="Alvin Zablan" />
+      		</div>
+      	);
+    }
+});
+
+ReactDOM.render(React.createElement(ShleemStream), document.body);
